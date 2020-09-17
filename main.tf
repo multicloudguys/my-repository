8fccdae0cd99ce7bcc6d17ac020614a083f3d495
multicloudguys <71434376+multicloudguys@users.noreply.github.com>
2020-09-17 15:01:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  credentials = file("account.json")
-  project     = "my-project-id"
+  credentials = file("oxygen-blue-273304-849927854726.json")
+  project     = "oxygen-blue-273304"
   region      = "us-central1"
   zone        = "us-central1-c"
 }
